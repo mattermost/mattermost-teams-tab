7e471a0c2958c86a01bb234d199d80237a521002
Jesse Hallam <jesse.hallam@gmail.com>
2024-08-26 18:49:08 -0300

    uniform whitespace

--- src/components/IncidentDetails/StepAccordion.tsx
+++ src/components/IncidentDetails/StepAccordion.tsx
@@ -2,41 +2,41 @@ import React from 'react';
 import { makeStyles, tokens, AccordionItem, AccordionHeader, AccordionPanel, Text } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
-    stepHeader: {
-        display: 'flex',
-        alignItems: 'baseline',
-        gap: '8px',
-    },
-    stepProgress: {
-        color: tokens.colorNeutralForeground3,
-    },
-    stepAccordion: {
-        backgroundColor: tokens.colorNeutralBackground6,
-    }
+  stepHeader: {
+    display: 'flex',
+    alignItems: 'baseline',
+    gap: '8px',
+  },
+  stepProgress: {
+    color: tokens.colorNeutralForeground3,
+  },
+  stepAccordion: {
+    backgroundColor: tokens.colorNeutralBackground6,
+  }
 });
 
 interface StepAccordionProps {
-    value: string;
-    headerText: string;
-    progressText: string;
-    children: React.ReactNode;
+  value: string;
+  headerText: string;
+  progressText: string;
+  children: React.ReactNode;
 }
 
 const StepAccordion: React.FC<StepAccordionProps> = ({ value, headerText, progressText, children }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <AccordionItem value={value}>
-            <AccordionHeader size='large' className={classes.stepAccordion}>
-                <div className={classes.stepHeader}>
-                    {headerText} <Text className={classes.stepProgress}>{progressText}</Text>
-                </div>
-            </AccordionHeader>
-            <AccordionPanel>
-                <div>{children}</div>
-            </AccordionPanel>
-        </AccordionItem>
-    );
+  return (
+    <AccordionItem value={value}>
+      <AccordionHeader size='large' className={classes.stepAccordion}>
+        <div className={classes.stepHeader}>
+          {headerText} <Text className={classes.stepProgress}>{progressText}</Text>
+        </div>
+      </AccordionHeader>
+      <AccordionPanel>
+        <div>{children}</div>
+      </AccordionPanel>
+    </AccordionItem>
+  );
 };
 
 export default StepAccordion;
--- src/components/IncidentDetails/TaskItem.tsx
+++ src/components/IncidentDetails/TaskItem.tsx
@@ -2,39 +2,39 @@ import React from 'react';
 import { makeStyles, Checkbox, Avatar, Text, tokens } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
-    taskAssignee: {
-        display: 'flex',
-        gap: '8px',
-        alignItems: 'center',
-        marginLeft: '40px',
-    },
-    taskItem: {
-        display: "flex",
-        paddingTop: '16px',
-        gap: '8px',
-        flexDirection: 'column',
-        color: tokens.colorNeutralForeground3,
-    }
+  taskAssignee: {
+    display: 'flex',
+    gap: '8px',
+    alignItems: 'center',
+    marginLeft: '40px',
+  },
+  taskItem: {
+    display: "flex",
+    paddingTop: '16px',
+    gap: '8px',
+    flexDirection: 'column',
+    color: tokens.colorNeutralForeground3,
+  }
 });
 
 interface TaskItemProps {
-    label: string;
-    avatarName: string;
-    assigneeText: string;
+  label: string;
+  avatarName: string;
+  assigneeText: string;
 }
 
 const TaskItem: React.FC<TaskItemProps> = ({ label, avatarName, assigneeText }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.taskItem}>
-            <Checkbox label={label} size='large' />
-            <div className={classes.taskAssignee}>
-                <Avatar size={24} shape={'circular'} name={avatarName} />
-                <Text size={200}>{assigneeText}</Text>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.taskItem}>
+      <Checkbox label={label} size='large' />
+      <div className={classes.taskAssignee}>
+        <Avatar size={24} shape={'circular'} name={avatarName} />
+        <Text size={200}>{assigneeText}</Text>
+      </div>
+    </div>
+  );
 };
 
 export default TaskItem;
